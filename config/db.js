c024344d0a199eced217939ef5cb6fee010826af
Hernan Artesi <hernan.artesi@willdom.com>
2024-11-02 14:40:03 -0300

    port

--- config/db.js
+++ config/db.js
@@ -4,7 +4,8 @@ require('dotenv').config()
 
 const dbConnection = new Sequelize(process.env.MYSQL_DATABASE, 'root',process.env.MYSQL_ROOT_PASSWORD, {
     host: process.env.MYSQL_URL,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    port: process.env.MYSQLPORT
 })
 
 module.exports = {
